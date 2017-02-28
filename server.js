30eb24cf1173bd9bdfe9aa1171a648805d1ec913
usrb4u <usrb4u@users.noreply.github.com>
2017-02-28 06:42:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,9 +31,11 @@ app.use(bodyParser.json());
 app.set('view engine','html');
 app.use('/',routes);
 
+
+
 const wss = new WebSocket.Server({ server,
     verifyClient:function(info,cb){
-        console.log('Initial acceptance:');
+        // console.log('Initial acceptance:');
         // console.log(info.req.connection);
         return cb(true);
     }
@@ -43,14 +45,68 @@ wss.on('connection', function connection(ws) {
   const location = url.parse(ws.upgradeReq.url, true);
   // You might use location.query.access_token to authenticate or share sessions
   // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+var resp='';
+var initial_packet = {
+    "PACKET_ID":"COMMON_CONFIG_DATA",
+    "GPS_EN_STATUS":"DIASBLE",
+    "DEVICEID":"XYZUVWCD123",
+    "CUSTOMER_ID":"CUST9926",
+    "DS_IPADDR":"192.168.1.12",
+    "DS_PORTNUMBER":1025,
+    "TS_IPADDR":ws._socket.remoteAddress,
+    "TS_PORTNUMBER":21,
+    "PROTOCOL_SELECTED":0,
+    "DATA_FORMAT":0
+}
+ 
+ eventEmitter.on('COMMON_CONFIG_SUCCESS',function(){
+     ws.send(JSON.stringify({"PACKET_ID":"DEV_ID_REQUEST"}));
+ });
 
-//   console.log(ws);
-console.log(ws._socket.remoteAddress);
-// console.log(location);
-ws.send('After acceptance from server')
+eventEmitter.on('DEV_ID_REQUEST',function(){
+     ws.send(JSON.stringify({"PACKET_ID":"DEV_ID_REQUEST"}));
+ });
+ 
+ eventEmitter.on('DEV_REG_REQUEST',function(){
+     ws.send(JSON.stringify({"PACKET_ID":"DEV_ID_REQUEST"}));
+ });
+ 
+ eventEmitter.on('COMMON_CONFIG_SUCCESS',function(){
+     ws.send(JSON.stringify({"PACKET_ID":"DEV_ID_REQUEST"}));
+ });
+ 
+ eventEmitter.on('COMMON_CONFIG_SUCCESS',function(){
+     ws.send(JSON.stringify({"PACKET_ID":"DEV_ID_REQUEST"}));
+ });
+ 
 
+    ws.send(JSON.stringify(initial_packet))
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+      resp = JSON.parse(message);
+      console.log(resp);
+      switch(resp.PACKET_ID) {
+    case 'COMMON_CONFIG_SUCCESS':
+        console.log(resp);
+        eventEmitter.emit('COMMON_CONFIG_SUCCESS');
+        break;
+    case 'DEV_ID_REQUEST':
+        console.log(resp);
+        eventEmitter.emit('DEV_ID_REQUEST');
+        break;
+    case 'UM_AIN1_CONFIG':
+        console.log(resp);
+        eventEmitter.emit('UM_AIN1_CONFIG');
+        break;
+    case 'UM_AIN2_CONFIG' :
+        console.log(resp);
+        eventEmitter.emit('UM_AIN2_CONFIG');
+        break;
+    case 'DEV_REG_REQUEST':
+        console.log(resp);
+        eventEmitter.emit('DEV_REG_REQUEST');
+    }
+       
+    // console.log('received: %s', message);
   });
 
 eventEmitter.on('hello', function(data){
@@ -64,7 +120,7 @@ eventEmitter.on('hello', function(data){
   })
   
 
-  ws.send(JSON.stringify({'hello':'world'}));
+//   ws.send(JSON.stringify({'hello':'world'}));
 });
 
 // app.get('/',function(req,res){
