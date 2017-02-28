1b885d6043e29d7b5bee6341d1623c4b6d33dca0
usrb4u <usrb4u@users.noreply.github.com>
2017-02-28 06:43:02 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,21 @@
 const WebSocket = require('ws');
+var events = require('events');
+var emtr = new events.EventEmitter();
 
 const ws = new WebSocket('ws://127.0.0.1:8080/');
+
+emtr.on('COMMON_CONFIG_DATA',function(){
+
+    ws.send(JSON.stringify({"PACKET_ID":"COMMON_CONFIG_SUCCESS"}));
+})
+
+emtr.on('DEV_ID_REQUEST',function(){
+    ws.send(JSON.stringify({"PACKET_ID":"REGDATA_RESPONSE","UM_AVAIL_STAT":"UM_PRESENT"}));
+})
+
+emtr.on('DEV_REG_REQUEST',function(){
+    ws.send(JSON.stringify({"PACKET_ID":"DEVID_RESPONSE","DEVID":"DEV1234"}))
+})
 
 ws.on('open', function open() {
   console.log('connected');
@@ -14,9 +29,30 @@ ws.on('close', function close() {
 
 ws.on('message', function incoming(data, flags) {
     console.log(data);
-    ws.on('msg',function outgoing(){
-        ws.send('Acknowledgement');
-    })
+    var pkt = JSON.parse(data);
+    console.log(pkt.PACKET_ID);
+    switch(pkt.PACKET_ID) {
+    case 'COMMON_CONFIG_DATA':
+        console.log(pkt);
+        emtr.emit('COMMON_CONFIG_DATA');
+        break;
+    case 'DEV_ID_REQUEST':
+        console.log(pkt);
+        emtr.emit('DEV_ID_REQUEST');
+        break;
+    case 'UM_AIN1_CONFIG':
+        console.log(pkt);
+        emtr.emit('UM_AIN1_CONFIG');
+        break;
+    case 'UM_AIN2_CONFIG' :
+        console.log(pkt);
+        emtr.emit('UM_AIN2_CONFIG');
+        break;
+    case 'DEV_REG_REQUEST':
+        console.log(pkt);
+        emtr.emit('DEV_REG_REQUEST');
+    }
+    
 //   console.log(`Roundtrip time: ${Date.now() - data} ms`, flags);
 
 //   setTimeout(function timeout() {
